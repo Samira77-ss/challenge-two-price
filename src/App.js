a30be97d11b7a2a580d5929eabe1536097b06ba9
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-02 11:49:14 +0330

    Added HTML structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,31 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">hi</div>
+      <div className="container">
+        <h1>Join our community 30-day</h1>
+        <h2>hassle-free money back guarantee</h2>
+        <p>
+          Gain access to our full library of tutorials along with expert code
+          reviews. Perfect for any developers who are serious about honing their
+          skills.
+        </p>
+        <div className="row">
+          <div className="col-6">
+            <h2>Monthly Subscription</h2>
+            <h3>$29 per month</h3>
+            <p>Full access for less than $1 a day</p>
+            <button className="btn btn-primary">Sugn Up</button>
+          </div>
+          <div className="col-6">
+            <h2>Why us</h2>
+            <p>
+              Tutorials by industry experts Peer & expert code review Coding
+              exercises Access to our GitHub repos Community forum Flashcard
+              decks New videos every week
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
