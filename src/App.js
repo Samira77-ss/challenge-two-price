b91ee43dc410b9f1889a49f07114ff2f54660e22
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-05 08:53:45 +0330

    added initial structures

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Join our community 30-day</h1>
-        <h2>hassle-free money back guarantee</h2>
-        <p>
-          Gain access to our full library of tutorials along with expert code
-          reviews. Perfect for any developers who are serious about honing their
-          skills.
-        </p>
+        <div className="community">
+          {" "}
+          <h1>Join our community</h1>
+          <h2>30 day,hassle-free money back guarantee</h2>
+          <p>
+            Gain access to our full library of tutorials along with expert code
+            reviews. Perfect for any developers who are serious about honing
+            their skills.
+          </p>
+        </div>
+
         <div className="row">
-          <div className="col-6">
-            <h2>Monthly Subscription</h2>
-            <h3>$29 per month</h3>
+          <div className="col-6 subscription">
+            <h3>Monthly Subscription</h3>
+            <span className="price">$29 </span> {""}
+            <span className="monthly"> per month</span>
             <p>Full access for less than $1 a day</p>
-            <button className="btn btn-primary">Sugn Up</button>
+            <button className="btn ">Sign Up</button>
           </div>
-          <div className="col-6">
-            <h2>Why us</h2>
+          <div className="col-6 contact">
+            <h3>Why us</h3>
             <p>
               Tutorials by industry experts Peer & expert code review Coding
               exercises Access to our GitHub repos Community forum Flashcard
