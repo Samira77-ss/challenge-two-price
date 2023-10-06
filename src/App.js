823105606e31a49fa40724ad82939c47d0a669fd
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-06 14:44:52 +0330

    update new changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
         <div className="community">
           {" "}
           <h1>Join our community</h1>
-          <h2>30 day,hassle-free money back guarantee</h2>
+          <h2>30-day,hassle-free money back guarantee</h2>
           <p>
             Gain access to our full library of tutorials along with expert code
             reviews. Perfect for any developers who are serious about honing
